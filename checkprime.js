9605ff84460d382d48c710811d46e542b3444ab3
Abhishek-Baksi <abhisek.baksi@icloud.com>
2022-11-02 12:07:06 +0530

    now its correct

--- checkprime.js
+++ checkprime.js
@@ -1,12 +1,15 @@
 function check_prime(num){
     let count=0;
-    for(let i=0;i<=num;i++){
-        if(num%i){
-            return true;
-        }else{
-            return false;
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
         }
     }
+if(count==2){
+    return true;
+}else{
+    return false;
+}
 }
 let ans=check_prime(17);
 ans==true?console.log("Palindrome"):console.log("NA");
